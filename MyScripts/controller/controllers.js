4c480b86d90b4237893abd677b962dae2e1f6941
andreshg112 <andreshg112@gmail.com>
2016-03-19 15:28:10 -0500

    Consume ar-recibos correctamente

--- MyScripts/controller/controllers.js
+++ MyScripts/controller/controllers.js
@@ -1,4 +1,4 @@
-app.controller('contactosController', function ($scope, recibosService) {
+app.controller('RecibosCtrl', function ($scope, recibosService) {
     //VistaModelo
     $scope.recibo = {}; //Objeto Actual
     $scope.recibos = []; //Listado de Objetos
@@ -10,6 +10,22 @@ app.controller('contactosController', function ($scope, recibosService) {
         $scope.recibo = {};
     }
 
+
+    $scope.imprSelec = function (muestra){
+        var ficha = document.getElementById(muestra);
+        var ventimp = window.open(' ','popimpr');
+        ventimp.document.write(ficha.innerHTML);
+        ventimp.document.write(
+            '<link href="http://bootswatch.com/simplex/bootstrap.min.css" rel="stylesheet"/>'
+            + '<link href="css/styles.css" rel="stylesheet"/>'
+            );
+
+        ventimp.document.close();
+        ventimp.print();
+        ventimp.close();
+    }
+
+
     function loadRecords() {
         var promiseGet = recibosService.getAll();
         promiseGet.then(function (pl) {
@@ -19,6 +35,10 @@ app.controller('contactosController', function ($scope, recibosService) {
                     console.log('failure loading Contactos', errorPl);
                 });
     }
+    $scope.anular = function(recibo) {
+        recibo.estado = "anulado";
+        $scope.update(recibo);
+    };
     //Model popup events
     $scope.showadd = function () {
         initialize();
@@ -26,7 +46,6 @@ app.controller('contactosController', function ($scope, recibosService) {
         $('#regModal').modal('show');
     };
     $scope.cancel = function () {
-        console.log($scope.editMode);
         if (!$scope.editMode) {
             initialize();
         }
@@ -36,22 +55,31 @@ app.controller('contactosController', function ($scope, recibosService) {
         $scope.recibo = this.recibo;
         $('#viewModal').modal('show');
     };
-    $scope.valorEnLetras = function() {
-        return valorEnLetras($scope.recibo.valor);
+    $scope.valorLetras = function() {
+        if ($scope.recibo.valor == 0) {
+            $scope.recibo.valor_letras = "CERO PESOS";
+        } else if($scope.recibo.valor == undefined) {
+            $scope.recibo.valor_letras = "";
+        } else {
+            $scope.recibo.valor_letras = valorEnLetras($scope.recibo.valor);
+        }
+        
     };
     $scope.showconfirm = function () {
         $scope.Product = this.product;
         $('#confirmModal').modal('show');
     };
     $scope.edit = function (recibo) {
-        $scope.recibo = recibo;
+        var editRecibo = recibo;
+        $scope.recibo = editRecibo;
         $scope.editMode = true;
         $('#regModal').modal('show');
     };
     //Function to Submit the form
     $scope.add = function () {
-        var promisePost = recibosService.post($scope.recibo);
-        promisePost.then(function (d) {
+        var promise = recibosService.post($scope.recibo);
+        promise.then(function (d) {
+            alert(d.data.mensaje);
             loadRecords();
         }, function (err) {
             alert("Some Error Occured " + JSON.stringify(err));
@@ -62,15 +90,11 @@ app.controller('contactosController', function ($scope, recibosService) {
         initialize();
     };
     //Functin Para Actualizar
-    $scope.update = function () {
-        var Contacto = {};
-        Contacto.id = $scope.recibo.id;
-        Contacto.telefono = $scope.recibo.telefono;
-        Contacto.nombre = $scope.recibo.nombre;
-        var promise = recibosService.put(Contacto.id, Contacto);
+    $scope.update = function (recibo) {
+        var promise = recibosService.put(recibo.numero, recibo);
         promise.then(function (d) {
             loadRecords();
-            alert("Guardó los Datos");
+            alert(d.data.mensaje);
         }, function (err) {
             alert("Some Error Occured " + JSON.stringify(err));
         });
--- MyScripts/modulo/pModule.js
+++ MyScripts/modulo/pModule.js
@@ -1,19 +1,12 @@
 var app;
 (function () {
-    app = angular.module("productsModule", ['ngRoute']);
+    app = angular.module("recibosApp", ['ngRoute']);
     app.config(['$routeProvider',
         function ($routeProvider) {
             $routeProvider
-                    .when('/Productos/Ejemplo1', {
-                        templateUrl: 'MyScripts/view/Ejemplo1.html'
-                    })
-                    .when('/Productos/Ejemplo2', {
-                        templateUrl: 'MyScripts/view/Ejemplo2.html',
-                        controller: 'Ejemplo2Controller'
-                    })
-                    .when('/Contactos/', {
-                        templateUrl: 'MyScripts/view/contactosTemplate.html',
-                        controller: 'contactosController'
+                    .when('/Recibos/', {
+                        templateUrl: 'MyScripts/view/recibos.html',
+                        controller: 'RecibosCtrl'
                     });
         }]);
 })();
